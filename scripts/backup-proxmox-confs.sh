b9438d411d53d87fa10155b1429f196b7335b9f0
Jan Jastrow <mail@jastrow.me>
2024-10-20 15:01:14 +0200

    Gather proxmox files for backup

---
+++ scripts/backup-proxmox-confs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Backup folder
+BACKUP_MAIN="$HOME/backups"
+mkdir -p $BACKUP_MAIN
+
+# Copy iptables rules
+if [ -d "/etc/iptables/" ]; then
+	mkdir -p $BACKUP_MAIN/iptables
+	rm $BACKUP_MAIN/iptables/rules.*
+	sudo cp /etc/iptables/rules.v4 $BACKUP_MAIN/iptables/
+	sudo cp /etc/iptables/rules.v6 $BACKUP_MAIN/iptables/
+fi
+
+# Copy LXC confs
+if [ -h "/etc/pve/lxc/" ]; then
+	mkdir -p $BACKUP_MAIN/lxc
+	rm $BACKUP_MAIN/lxc/*.conf
+	sudo cp -R /etc/pve/lxc/* $BACKUP_MAIN/lxc/
+fi
+
+# Change owner
+sudo chown -R $USER:$USER $BACKUP_MAIN
