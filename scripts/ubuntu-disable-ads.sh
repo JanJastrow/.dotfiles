ef1ed79071e5d4517f71dd06bc9685b8e2f7ad83
Jan Jastrow <mail@jastrow.me>
2024-10-30 22:28:03 +0100

    Also here

--- scripts/ubuntu-disable-ads.sh
+++ scripts/ubuntu-disable-ads.sh
@@ -3,3 +3,6 @@ set -uo pipefail
 
 # Comment out all URLs in this apt source
 sudo sed -i'' -e 's/^\(\s\+\)\([^#]\)/\1# \2/' /etc/apt/apt.conf.d/20apt-esm-hook.conf
+
+# Also here
+sudo sed -i'' -e '/^[^#]/ s/^/# /' /var/lib/ubuntu-advantage/apt-esm/etc/apt/sources.list.d/ubuntu-esm-apps.sources
